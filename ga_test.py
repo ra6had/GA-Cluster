288c5bc94a94d2d090e468b527be9100d9a65df5
Rashad <46797873+ra6had@users.noreply.github.com>
2019-11-14 01:27:21 +0300

    testing score measures

--- ga_test.py
+++ ga_test.py
@@ -2,24 +2,29 @@ import galuster
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans  as km
+import scipy.spatial.distance as dist
 
 
 df = pd.read_csv('sample.csv').set_index('OA')
 X = df.values
 
-kmeans = km(n_clusters=8).fit_predict(X)
+#kmeans = km(n_clusters=8).fit_predict(X)
 
 
 
-pop = galuster.Population(10, 'means', n_clusters=5, n_variables=10)
+pop = galuster.Population(1000, 'means', n_clusters=5, n_variables=10)
 
 score = []
+means = []
 if pop.ch_type == 'means':
-    n_clusters = len(pop.pop[0])
-    for chromosome in pop.pop:
-        kmeans = km(n_clusters, chromosome).fit(X)
-        centers = kmeans.cluster_centers_
-        clusters = kmeans.predict(X)
-        for element in clusters:
-            dist = np.linalg.norm(centers[element], X[index(element)])
-            
+	n_clusters = len(pop.pop[0])
+	for chromosome in pop.pop:
+		kmeans = km(n_clusters, chromosome).fit(X)
+		centers = kmeans.cluster_centers_
+		means.append(centers)
+		clusters = kmeans.predict(X)
+		distances = []
+		for i in range(len(clusters)):
+			distance = dist.euclidean(centers[clusters[i]], X[i])
+			distances.append(distance)
+		score.append(sum(distances))
