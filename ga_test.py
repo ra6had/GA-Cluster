0e1cb27229329bc65ce96f445242450fd1842d43
Rashad <46797873+ra6had@users.noreply.github.com>
2019-11-13 14:49:12 +0300

    Add files via upload

--- ga_test.py
+++ ga_test.py
@@ -1,9 +1,25 @@
 import galuster
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans  as km
 
-#x = galuster.MeanGene(3,6)
-#print(x.gene)
-#
-y = galuster.VarGene(167, 60)
-print(y.gene)
 
-#pop = galuster.Population(10, g_type='means', n_variables=10, n_clusters=5)
+df = pd.read_csv('sample.csv').set_index('OA')
+X = df.values
+
+kmeans = km(n_clusters=8).fit_predict(X)
+
+
+
+pop = galuster.Population(10, 'means', n_clusters=5, n_variables=10)
+
+score = []
+if pop.ch_type == 'means':
+    n_clusters = len(pop.pop[0])
+    for chromosome in pop.pop:
+        kmeans = km(n_clusters, chromosome).fit(X)
+        centers = kmeans.cluster_centers_
+        clusters = kmeans.predict(X)
+        for element in clusters:
+            dist = np.linalg.norm(centers[element], X[index(element)])
+            
--- galuster.py
+++ galuster.py
@@ -1,52 +1,69 @@
 import numpy as np
+from sklearn.cluster import KMeans as km
 
 
-class MeanGene:
+class MeanChrom:
 	def __init__(self, n_clusters, n_variables):
 		self.n_clusters = n_clusters
 		self.n_variables = n_variables
 
-		self.gene = np.random.random((self.n_clusters, self.n_variables))
+		self.chrom = np.random.random((self.n_clusters, self.n_variables))
 
 	def __str__(self):
-		return "This is the meanest gene ever!"
+		return "This is the meanest chromosome ever!"
 
-class VarGene:
+class VarChrom:
 	def __init__(self, n_variables, n_features):
 		self.n_variables = n_variables
 		self.n_features = n_features
 		x = n_variables - n_features
 		zeros = np.zeros(x)
 		ones = np.ones(self.n_features)
-		self.gene = np.hstack((zeros, ones))
-		np.random.shuffle(self.gene)
+		self.chrom = np.hstack((zeros, ones)).astype(int)
+		np.random.shuffle(self.chrom)
 
 
 	def __str__(self):
-		return "This is the most variable gene ever!"
+		return "This is the most variable chromosome ever!"
 
 class Population:
-	def __init__(self, size, g_type='means', **kwargs):
+	def __init__(self, size, ch_type='means', **kwargs):
 		self.size = size
-		self.g_type = g_type
+		self.ch_type = ch_type
 		self.pop = []
 
-		for gene in range(self.size):
+		for chrom in range(self.size):
 
-			if self.g_type != 'means' and self.g_type != 'variables':
-				raise ValueError('Please input a valid gene type n\
-					 Supported gene types are "means" and "variables"')
+			if self.ch_type != 'means' and self.ch_type != 'variables':
+				raise ValueError('Please input a valid chromosome type n\
+					 Supported chromosome types are "means" and "variables"')
 				break
-			elif self.g_type == 'means':
-				gene = MeanGene(kwargs['n_clusters'], kwargs['n_variables'])
-				self.pop.append(gene.gene)
-			elif self.g_type == 'variables':
-				gene = VarGene(kwargs['n_variables'], kwargs['n_features'])
-				self.pop.append(gene.gene)
+			elif self.ch_type == 'means':
+				chrom = MeanChrom(kwargs['n_clusters'], kwargs['n_variables'])
+				self.pop.append(chrom.chrom)
+			elif self.ch_type == 'variables':
+				chrom = VarChrom(kwargs['n_variables'], kwargs['n_features'])
+				self.pop.append(chrom.chrom)
 			else:
 				raise ValueError('Make sure you insert the appropriate kwargs')
 
 	def __str__(self):
 		return "This is the population"
 
-	#def mutate(self, )
+	
+#    def evaluate(self, X):
+#        score = []
+#        if self.ch_type == 'means':
+#            n_clusters = len(self.pop[0])
+#            for chromosome in self.pop:
+#                kmeans = km(n_clusters, chromosome).fit(X)
+#                centers = kmeans.cluster_centers_
+#                clusters = predict(X)
+#    
+#    def select(self, prc=0.5):
+#        
+#        score = []
+#        if self.ch_type == 'means':
+#            for chromosome in self.pop:
+#                
+    
